c3d32b3c33cf138d2b2083e5bb3e806f4b54b87c
not-ra1n <142693849+not-ra1n@users.noreply.github.com>
2024-01-05 03:31:48 -0500

    Make a prompt asking how many squares per side

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,26 +1,10 @@
 const container = document.querySelector('#container');
 
-//const row1 = document.querySelector('#row1')
-//const row2 = document.querySelector('#row2')
-//const row3 = document.querySelector('#row3')
-//const row4 = document.querySelector('#row4')
-//const row5 = document.querySelector('#row5')
-//const row6 = document.querySelector('#row6')
-//const row7 = document.querySelector('#row7')
-//const row8 = document.querySelector('#row8')
-//const row9 = document.querySelector('#row9')
-//const row10 = document.querySelector('#row10')
-//const row11 = document.querySelector('#row11')
-//const row12 = document.querySelector('#row12')
-//const row13 = document.querySelector('#row13')
-//const row14 = document.querySelector('#row14')
-//const row15 = document.querySelector('#row15')
-//const row16 = document.querySelector('#row16')
-
+let number = 16;
 
 function generateContent(e) {
     let fragment = new DocumentFragment();
-    let numberOfItems = 256;
+    let numberOfItems = number*number;
   
     for (let i = 0; i < numberOfItems; i++) {
   
@@ -36,7 +20,15 @@ function generateContent(e) {
     container.appendChild(fragment);
   }
 
-  container.appendChild(generateContent());
+generateContent();
 
-  
+function changeGlobal(newVal) {
+  number = newVal; // updating the value of the global variable
+}
 
+const change = document.querySelector('#change-square');
+  change.addEventListener('click', () => {
+    changeSquare = prompt("How many squares do you want per side?");
+    changeGlobal(changeSquare);
+    generateContent();
+  });
